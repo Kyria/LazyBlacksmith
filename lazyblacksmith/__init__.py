f30a99c042c476baf20695cc566797c1db0bba86
Guillaume <anakhon@gmail.com>
2015-05-10 15:35:36 +0200

    Add CSRF protection

--- lazyblacksmith/__init__.py
+++ lazyblacksmith/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from lazyblacksmith.models import db
+from flask_wtf.csrf import CsrfProtect
 
 import config
 
@@ -11,3 +12,9 @@ app.config.from_object(config)
 # no app object passed! Instead we use use db.init_app in the factory.
 db.app = app
 db.init_app(app)
+
+# csrf
+csrf = CsrfProtect()
+csrf.init_app(app)
+
+from views import index
