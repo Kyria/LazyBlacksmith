9b0e221c2a45e71159c782db43b0d371fec0debb
Guillaume <anakhon@gmail.com>
2015-05-25 18:21:18 +0200

    Add methods

--- lazyblacksmith/models/eve_sde/item.py
+++ lazyblacksmith/models/eve_sde/item.py
@@ -1,11 +1,14 @@
 from . import db
+from .activity import Activity
+from flask import url_for
+
 
 class Item(db.Model):
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     name = db.Column(db.String(100), nullable=True)
     max_production_limit = db.Column(db.Integer, nullable=True)
-    
+
     # foreign keys
     activities = db.relationship('Activity', backref='blueprint', lazy='dynamic')
     activity_products = db.relationship('ActivityProduct', backref='blueprint', lazy='dynamic', foreign_keys='ActivityProduct.item_id')
@@ -16,3 +19,15 @@ class Item(db.Model):
     skill_for_activities = db.relationship('ActivitySkill', backref='skill', lazy='dynamic', foreign_keys='ActivitySkill.skill_id')
     material_for_activities = db.relationship('ActivityMaterial', backref='material', lazy='dynamic', foreign_keys='ActivityMaterial.material_id')
     
+    def icon_32(self):
+        static_url = "ccp/Types/%d_32.png" % self.id
+        return url_for('static', filename=static_url)
+
+    def icon_64(self):
+        static_url = "ccp/Types/%d_64.png" % self.id
+        return url_for('static', filename=static_url)
+
+    def is_manufactured(self):
+        if self.product_for_activities.filter_by(activity=Activity.ACTIVITY_MANUFACTURING).count() > 0:
+            return True
+        return False
