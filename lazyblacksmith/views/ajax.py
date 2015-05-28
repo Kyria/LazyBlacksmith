9d745a8eab1024c518779a34f0e0d1d2774d19f8
Guillaume <anakhon@gmail.com>
2015-05-28 20:19:19 +0200

    Add cache to ajax requests

--- lazyblacksmith/views/ajax.py
+++ lazyblacksmith/views/ajax.py
@@ -8,6 +8,7 @@ from flask import request
 from sqlalchemy.orm import aliased
 from sqlalchemy.orm.exc import NoResultFound
 
+from lazyblacksmith.cache import cache
 from lazyblacksmith.models import Item
 from lazyblacksmith.models import Activity
 from lazyblacksmith.models import ActivityProduct
@@ -16,6 +17,14 @@ from lazyblacksmith.models import SolarSystem
 
 ajax = Blueprint('ajax', __name__)
 
+def is_not_ajax():
+    """ 
+    Return True if request is not ajax
+    This function is used in @cache annotation to not cache direct call (http 403)
+    """
+    return not request.is_xhr
+
+
 @ajax.route('/blueprint/search/<string:name>', methods=['GET'])
 def blueprint_search(name):
     """
@@ -23,26 +32,28 @@ def blueprint_search(name):
     name is the request name.
     """
     if request.is_xhr:
-        #cache_key = 'blueprint:search:%s' % (name.lower().replace(" ", ""),)
+        cache_key = 'blueprint:search:%s' % (name.lower().replace(" ", ""),)
 
-        data = None #cache.get(cache_key)
+        data = cache.get(cache_key)
         if data is None:
-            blueprints = Item.query.filter(Item.name.ilike('%'+name.lower()+'%'), Item.max_production_limit.isnot(None)).order_by(Item.name.asc()).all()
+            blueprints = Item.query.filter(
+                Item.name.ilike('%'+name.lower()+'%'),
+                Item.max_production_limit.isnot(None)
+            ).order_by(
+                Item.name.asc()
+            ).all()
 
             data = []
             for bp in blueprints: 
                 invention = 0
-                
-                ## we are taking the first activity as we cannot have RE and invention at the same time
-                #for pa in ActivityProduct.objects.filter(product_id = bp.item_id):
-                #    if pa.activity == Activity.ACTIVITY_INVENTION:
-                #        invention = 1
-                #        break
-                        
+                                      
                 data.append([bp.id, bp.name, invention])
             
             # cache for 7 day as it does not change that often
-            #cache.set(cache_key, json.dumps(data), 24*3600*7)
+            cache.set(cache_key, json.dumps(data), 24*3600*7)
+
+        else:
+            data = json.loads(data)
 
         return jsonify(result=data)
     else:
@@ -50,53 +61,55 @@ def blueprint_search(name):
 
 
 @ajax.route('/blueprint/bom/<int:blueprint_id>', methods=['GET'])
+@cache.memoize(timeout=3600*24*7, unless=is_not_ajax)
 def blueprint_bom(blueprint_id):
     """
     Return JSON with the list of all bill of material for
     each material in the blueprint given in argument
     """
     if request.is_xhr:
-        #cache_key = 'blueprint:matbom:%s' % item_id
-    
-        data = None #cache.get(cache_key)
-        if data is None:
-            blueprints = ActivityMaterial.query.filter_by(item_id=blueprint_id, activity=Activity.ACTIVITY_MANUFACTURING).all()
+        blueprints = ActivityMaterial.query.filter_by(
+            item_id=blueprint_id, 
+            activity=Activity.ACTIVITY_MANUFACTURING
+        ).all()
 
-            data = OrderedDict()
-            for bp in blueprints:
+        data = OrderedDict()
+        for bp in blueprints:
 
-                # As all item cannot be manufactured, catch the exception 
-                try:
-                    bp_final = bp.material.product_for_activities
-                    bp_final = bp_final.filter_by(activity=Activity.ACTIVITY_MANUFACTURING).one()
-                    bp_final = bp_final.blueprint
-                except NoResultFound:
-                    continue
+            # As all item cannot be manufactured, catch the exception 
+            try:
+                bp_final = bp.material.product_for_activities
+                bp_final = bp_final.filter_by(activity=Activity.ACTIVITY_MANUFACTURING).one()
+                bp_final = bp_final.blueprint
+            except NoResultFound:
+                continue
 
-                mats = bp_final.activity_materials.filter_by(activity=Activity.ACTIVITY_MANUFACTURING).all()
+            mats = bp_final.activity_materials.filter_by(activity=Activity.ACTIVITY_MANUFACTURING).all()
 
-                if bp_final.id not in data:
-                    data[bp_final.id] = {
-                        'id':bp.material.id,
-                        'icon':bp_final.icon_32(),
-                        'name':bp_final.name,
-                        'materials':[],
-                    }
+            if bp_final.id not in data:
+                data[bp_final.id] = {
+                    'id':bp.material.id,
+                    'icon':bp_final.icon_32(),
+                    'name':bp_final.name,
+                    'materials':[],
+                }
 
-                for mat in mats:
-                    data[bp_final.id]['materials'].append({
-                        'id':mat.material.id,
-                        'name':mat.material.name,
-                        'quantity':mat.quantity,
-                        'icon':mat.material.icon_32(),
-                    })
+            for mat in mats:
+                data[bp_final.id]['materials'].append({
+                    'id':mat.material.id,
+                    'name':mat.material.name,
+                    'quantity':mat.quantity,
+                    'icon':mat.material.icon_32(),
+                })
 
         return jsonify(result=data)
+
     else:
         return 'Cannot call this page directly', 403
 
 
 @ajax.route('/solarsystem/list', methods=['GET'])
+@cache.cached(timeout=3600*24*7, unless=is_not_ajax)
 def solarsystems():
     """
     Return JSON result with system list (ID,Name)
@@ -109,18 +122,3 @@ def solarsystems():
         return jsonify(result=data)
     else:
         return 'Cannot call this page directly', 403
-
-#if request.is_ajax():
-#cache_key = 'system:list'
-#data = cache.get(cache_key)
-#if data is None:
-#systems = System.objects.all()
-#data = []
-#for system in systems:
-#data.append(system.name)
-## cache for 1 day as it does not change that often
-#data = json.dumps(data)
-#cache.set(cache_key, data, 24*3600*7)
-#return HttpResponse(data, content_type='application/json')
-#else:
-#return HttpResponse(content='Cannot call this page directly', status=403)#
