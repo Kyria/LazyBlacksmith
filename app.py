21c3bb386a6ac03f424c216f8e2e41d7ef7cd2a6
Guillaume <anakhon@gmail.com>
2015-05-28 20:16:44 +0200

    Move to use an app factory

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from lazyblacksmith import app
+from lazyblacksmith.app_factory import create_app
 import config
 
+app = create_app(config)
 app.run(port=config.PORT, host=config.HOST)
---
+++ lazyblacksmith/app_factory.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CsrfProtect
+
+def create_app(config_object):
+    # app
+    app = Flask(__name__.split('.')[0])
+    app.config.from_object(config_object)
+
+    # blueprint stuff
+    from lazyblacksmith.views.ajax import ajax
+    from lazyblacksmith.views.blueprint import blueprint
+    from lazyblacksmith.views.home import home
+    from lazyblacksmith.utils.template_filter import templatefilter
+    app.register_blueprint(blueprint, url_prefix='/blueprint')
+    app.register_blueprint(ajax, url_prefix='/ajax')
+    app.register_blueprint(home)
+    app.register_blueprint(templatefilter)
+
+    # db
+    from lazyblacksmith.models import db
+    db.app = app
+    db.init_app(app)
+
+    # csrf
+    csrf = CsrfProtect()
+    csrf.init_app(app)
+
+    # cache
+    from cache import cache
+    cache.init_app(app)
+
+    # return app
+    return app
