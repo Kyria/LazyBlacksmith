6a79bbe8e0da0b2d52750d34909e1d8ec3b0aeb0
Guillaume <anakhon@gmail.com>
2015-06-02 19:27:00 +0200

    Manage static files with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,68 @@ module.exports = function (grunt) {
                 ]
             },
         },
+
+        concat: {
+            bootstrap: {
+                src: [                    
+                    'lazyblacksmith/static/js/bootstrap/modal.js',
+                    'lazyblacksmith/static/js/bootstrap/tooltip.js',
+                    'lazyblacksmith/static/js/bootstrap/popover.js',
+                    'lazyblacksmith/static/js/bootstrap/button.js',
+                    'lazyblacksmith/static/js/bootstrap/collapse.js',
+                    'lazyblacksmith/static/js/bootstrap/dropdown.js',
+                    'lazyblacksmith/static/js/bootstrap/tab.js',
+                    'lazyblacksmith/static/js/bootstrap/affix.js',
+                    'lazyblacksmith/static/js/bootstrap/alert.js',
+                    'lazyblacksmith/static/js/bootstrap/carousel.js',
+                    'lazyblacksmith/static/js/bootstrap/transition.js',
+                    'lazyblacksmith/static/js/bootstrap/scrollspy.js',
+                    'lazyblacksmith/static/js/bootstrap-addons/*.js'
+
+                ],
+                dest: 'lazyblacksmith/static/js/bootstrap.js'
+            }
+        },
+        uglify: {
+            combined: {
+                options: {
+                    sourceMap: true
+                },
+                src: [
+                    'lazyblacksmith/static/js/bootstrap.js',
+                    'lazyblacksmith/static/js/jquery/*.js',
+                    'lazyblacksmith/static/js/misc/*.js',
+                    'lazyblacksmith/static/js/lazyblacksmith.js',
+                    'lazyblacksmith/static/js/lazyblacksmith.*.js',
+                ],
+                dest: 'lazyblacksmith/static/js/lazybl-combined.min.js'
+            }
+        },
+        less: {
+            options: {
+                cleancss: true,
+                compress: true,
+                modifyVars: {
+                    'icon-font-path': '"../fonts/"',
+                    'fa-font-path': '"../fonts"',
+                    'ui-image-dir': '"../css/jquery-ui-images"',
+                }
+            },
+            themes: {
+                files: {
+                    'lazyblacksmith/static/css/theme-default.min.css': 'lazyblacksmith/static/less/theme-default/bootstrap.less',
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-flake8');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'uglify', 'less']);
 
     grunt.loadNpmTasks('grunt-flake8');
 };
