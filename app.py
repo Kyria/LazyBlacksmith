df2c56ff498e48d6303b49a5850f898749dc0394
Guillaume <anakhon@gmail.com>
2015-06-02 18:25:08 +0200

    Add things for uwsgi

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ from lazyblacksmith.app_factory import create_app
 import config
 
 app = create_app(config)
-app.run(port=config.PORT, host=config.HOST)
+
+if __name__ == '__main__':
+	app.run(port=config.PORT, host=config.HOST)
