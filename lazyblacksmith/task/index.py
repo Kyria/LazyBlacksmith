0478c3f4ef197db1e99e99447696833ee9fd9e69
Guillaume <anakhon@gmail.com>
2015-06-22 12:57:03 +0200

    Add missing init and correct few things

--- lazyblacksmith/task/index.py
+++ lazyblacksmith/task/index.py
@@ -2,35 +2,35 @@
 import pycrest
 import datetime
 
-import lazyblacksmith.utils.crestutils
 from lazyblacksmith.models import db
 from lazyblacksmith.models import IndustryIndex
+from lazyblacksmith.utils.crestutils import get_crest
+from lazyblacksmith.utils.crestutils import get_all_items
+from lazyblacksmith.utils.crestutils import get_by_attr
 
 def get_industry_index():
-    """
-    Connect to the public CREST and get the industry indexes list.
-    """
-    crest = crestutils.get_crest()
+    """ Connect to the public CREST and get the industry indexes list. """
+    crest = get_crest()
     industry_system_page = crest.industry.systems()
 
     insert_index_list = []
 
     # get all indexes
-    all_indexes = crestutils.get_all_items(industry_system_page)
+    all_indexes = get_all_items(industry_system_page)
 
     # TODO: get the date from headers
     # check if the data are not still from cache and if we don't already have them.  
-    date = datetime.utcnow()
+    date = datetime.datetime.utcnow()
 
     for index in all_indexes:
-        solar_system = industry_system_page.solarSystem.id
+        solar_system = index.solarSystem.id
 
         for activity_index in index.systemCostIndices:
             row = {}
             row['solarsystem_id'] = solar_system
             row['date'] = date
-            row['activity'] = index.solarSystem.id
-            row['cost_index'] = index.solarSystem.id
+            row['activity'] = activity_index.activityID
+            row['cost_index'] = activity_index.costIndex
             insert_index_list.append(row)
 
     db.engine.execute(
