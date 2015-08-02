864cf50cc543f53b21fced877baa3bdf4f582997
Guillaume <anakhon@gmail.com>
2015-08-03 00:19:08 +0200

    Add util function for utcnow with pytz

---
+++ lazyblacksmith/utils/time.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+import pytz
+from datetime import datetime
+
+def utcnow():
+    utc_now = datetime.utcnow()
+    utc_now = utc_now.replace(tzinfo=pytz.utc)
+    return utc_now
