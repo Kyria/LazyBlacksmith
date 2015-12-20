a5af21d04400b2834d872ad161eb91e180d641c6
Kyia <anakhon@gmail.com>
2015-12-20 15:57:27 +0100

    use public crest and no authed crest
    + fixes

--- lazyblacksmith/utils/crestutils.py
+++ lazyblacksmith/utils/crestutils.py
@@ -2,7 +2,7 @@
 import config
 
 from lazyblacksmith.utils.pycrest import EVE
-from lazyblacksmith.cache import cache
+from lazyblacksmith.extension.cache import cache
 
 from flask import url_for
 
@@ -18,31 +18,27 @@ def get_crest():
     return crest
 
 def get_by_attr(objlist, attr, val):
-    ''' Searches list of dicts for a dict with dict[attr] == val '''
+    """ Searches list of dicts for a dict with dict[attr] == val """
     matches = [getattr(obj, attr) == val for obj in objlist]
     index = matches.index(True)  # find first match, raise ValueError if not found
     return objlist[index]
 
 def get_all_items(page):
-    ''' Fetch data from all pages '''
-    ret = page().items
-    while hasattr(page(), 'next'):
-        page = page().next()
-        ret.extend(page().items)
+    """ Fetch data from all pages """
+    ret = page.items
+    while hasattr(page, 'next'):
+        page = page.next()
+        ret.extend(page.items)
     return ret
 
 @cache.cached(timeout=3600*24)
 def get_adjusted_price():
-    crest = None
-    if current_user.is_authenticated:
-        crest = current_user.get_authed_crest()
-    else:
-        crest = get_crest()
+    crest = get_crest()
 
     item_adjusted_price = {}
 
     marketPrice = get_all_items(crest.marketPrices())
     for itemPrice in marketPrice:
-        item_adjusted_price[marketPrice['type']['id']] = marketPrice['adjustedPrice']
+        item_adjusted_price[itemPrice.type.id] = itemPrice.adjustedPrice
 
     return item_adjusted_price
