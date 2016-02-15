7b65c32d9a6e34406ee2af6c27fbc163da0b39f2
Guillaume <anakhon@gmail.com>
2016-02-15 19:53:07 +0100

    Remove celery_crontab.py

--- celery_app.py
+++ celery_app.py
@@ -1,11 +1,22 @@
 # -*- encoding: utf-8 -*-
 import config
 
+from celery.schedules import crontab
 from lazyblacksmith.app import create_app
 from lazyblacksmith.extension.celery_app import celery_app
 
-import celery_crontab
-
 app = create_app(config)
 app.app_context().push()
-celery_app.conf.update(celery_crontab)
+
+celery_app.conf.update({
+    'CELERYBEAT_SCHEDULE': {
+        'adjusted-price': {
+            'task': 'lazyblacksmith.tasks.adjusted_price.get_adjusted_price',
+            'schedule': crontab(hour=1, minute=30),
+        },
+        'item-market-price': {
+            'task': 'lazyblacksmith.tasks.market_order.update_market_price',
+            'schedule': crontab(hour='*', minute=00),
+        },
+    }
+})
