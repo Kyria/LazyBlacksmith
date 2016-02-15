f032873bfd3a6c887f9757e83b2b64cf8c3ce3f1
Guillaume <anakhon@gmail.com>
2016-02-15 13:33:55 +0100

    fix import

--- celery_app.py
+++ celery_app.py
@@ -2,7 +2,7 @@
 import config
 
 from lazyblacksmith.app import create_app
-from lazyblacksmith.celery_app import celery_app
+from lazyblacksmith.extension.celery_app import celery_app
 
 import celery_crontab
 
