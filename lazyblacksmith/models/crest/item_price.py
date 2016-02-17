9baadfedcc27218ea9b45be16a5ba2743a9c33cc
Guillaume <anakhon@gmail.com>
2016-02-17 20:03:30 +0100

    Remove FK, as it slower import
    and we don't really need them, really.

--- lazyblacksmith/models/crest/item_price.py
+++ lazyblacksmith/models/crest/item_price.py
@@ -6,9 +6,8 @@ from sqlalchemy import func
 
 
 class ItemPrice(db.Model):
-
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
-    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    item_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    region_id = db.Column(db.Integer)
     update_at = db.Column(UTCDateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     sell_price = db.Column(db.Numeric(precision=17, scale=2, decimal_return_scale=2), nullable=True)
     buy_price = db.Column(db.Numeric(precision=17, scale=2, decimal_return_scale=2), nullable=True)
