5323eaf0d208ad7e6db8730e3c565206181ff2b2
Guillaume <Kyria@users.noreply.github.com>
2016-08-02 16:09:36 +0200

    Change price order from CREST to use the new endpoint
    Also remove gevent which doesn't help that much anymore !
    Close #12

--- lazyblacksmith/tasks/market_order.py
+++ lazyblacksmith/tasks/market_order.py
@@ -13,63 +13,61 @@ from lazyblacksmith.utils.crestutils import get_crest
 from lazyblacksmith.utils.pycrest.errors import APIException
 from requests.exceptions import Timeout
 
-import gevent
-
-from gevent.pool import Pool
-
 from ratelimiter import RateLimiter
 
-from gevent import monkey
-
-monkey.patch_all()
+# divide by 2, as we may have mulitple pages in some cases (it still does 75/sec)
 rate_limiter = RateLimiter(max_calls=config.CREST_REQ_RATE_LIM / 2, period=1)
 
-raw_sql_query = """
-    INSERT INTO %s (item_id, region_id, sell_price, buy_price)
-    VALUES (%s, %s, %s, %s)
-    ON DUPLICATE KEY UPDATE sell_price = %s, buy_price = %s
-"""
-
-
-def crest_order_price(market_crest_url, type_url, item_id, region):
+def crest_order_price(region_crest, region_id, item_id_list):
     """
     Get and return the orders <type> (sell|buy) from
-    a given region for a given type
+    a given region
     """
 
     # call the crest page and extract all items from every pages if required
-    try:
-        buy_orders_crest = get_all_items(market_crest_url.marketBuyOrders(type=type_url))
-        sell_orders_crest = get_all_items(market_crest_url.marketSellOrders(type=type_url))
-    except APIException as api_e:
-        if "503" in str(api_e):
-            print "[%s] Error 503 happened ! Item ID: %s" % (time.strftime("%x %X"), item_id)
+    orders_crest = get_all_items(region_crest.marketOrdersAll())
+
+    item_list = {'update':{}, 'insert': {}}
+    for order in orders_crest:
+        item_id = order.type
+        
+        # values if we already have this item in database or not
+        if item_id in item_id_list:
+            stmt_type = 'update' 
+            region_id_label = 'u_region_id'
+            item_id_label = 'u_item_id' 
         else:
-            print "%s Unexpected error. Item ID:  %s " % (time.strftime("%x %X"), item_id)
-        return None
-    except Timeout:
-        print "[%s] Error: timeout while getting price from crest ! Item ID: %s" % (time.strftime("%x %X"), item_id)
-        return None
+            stmt_type =  'insert'
+            region_id_label = 'region_id'
+            item_id_label = 'item_id'
+        
+        # do we already looped on this item ?
+        if item_id not in item_list:
+            item_list[stmt_type][item_id] = {
+                'sell_price': None,
+                'buy_price': 0,
+                region_id_label: region_id,
+                item_id_label: item_id,
+            }
+            
+        if not order.buy:
+            # if we have already set this item, but on buy order, set price
+            # because min() will always return "0" if we set it to 0 on init
+            if item_list[stmt_type][item_id]['sell_price'] is None:
+                item_list[stmt_type][item_id]['sell_price'] = order.price
+                continue
+            
+            item_list[stmt_type][item_id]['sell_price'] = min(
+                item_list[stmt_type][item_id]['sell_price'],
+                order.price
+            )
+        else:
+            item_list[stmt_type][item_id]['buy_price'] = max(
+                item_list[stmt_type][item_id]['buy_price'],
+                order.price
+            )
 
-    # if no orders found...
-    if not sell_orders_crest and not buy_orders_crest:
-        return
-
-    # extract min/max
-    sell_price = 0.0
-    if sell_orders_crest:
-        sell_price = (min(sell_orders_crest, key=lambda order: order.price)).price
-
-    buy_price = 0.0
-    if buy_orders_crest:
-        buy_price = (max(buy_orders_crest, key=lambda order: order.price)).price
-
-    return {
-        'item_id': item_id,
-        'sell_price': sell_price,
-        'buy_price': buy_price,
-        'region_id': region.id,
-    }
+    return item_list
 
 
 @celery_app.task(name="schedule.update_market_price")
@@ -82,46 +80,46 @@ def update_market_price():
         Region.id.in_(config.CREST_REGION_PRICE)
     ).all()
 
-    item_list = ItemAdjustedPrice.query.all()
-
-    # number in pool is the max concurrent session we can open (20).
-    greenlet_pool = Pool(20)
-    greenlet_list = []
-
-    # loop over regions
+    print "Total | Delta | Insert | Update | Region"
+    print "-----------------------------------------"
+    time_start = time.time()
+    
     for region in region_list:
-        market_crest = (get_by_attr(get_all_items(crest.regions()), 'name', region.name))()
+        time_delta = time.time()
+        
+        region_crest = get_all_items(crest.regions())
+        region_crest = get_by_attr(region_crest, 'name', region.name)
+        region_crest = region_crest()
+              
+        item_id_list = [it[0] for it in db.session.query(ItemPrice.item_id).filter_by(region_id=region.id)]
 
-        # loop over items
-        for item in item_list:
-            type_url = '%s%s/' % (item_type_url, item.item_id)
+        # use rate limited context to prevent too much call per seconds
+        with rate_limiter:
+            item_list = crest_order_price(region_crest, region.id, item_id_list)       
+        
+        # check if we have any update to do
+        if len(item_list['update']) > 0:
+            update_stmt = ItemPrice.__table__.update() \
+                                   .where(ItemPrice.item_id==db.bindparam('u_item_id')) \
+                                   .where(ItemPrice.region_id==db.bindparam('u_region_id'))
+            db.engine.execute(
+                update_stmt,
+                item_list['update'].values()
+            )
 
-            # use rate limited contexte to prevent too much greenlet spawn per seconds
-            with rate_limiter:
-                # greenlet spawn buy order getter
-                greenlet_list.append(
-                    greenlet_pool.spawn(
-                        crest_order_price,
-                        market_crest,
-                        type_url,
-                        item.item_id,
-                        region
-                    )
-                )
+        # check if we have any insert to do
+        if len(item_list['insert']) > 0:
+            # execute inserts
+            db.engine.execute(
+                ItemPrice.__table__.insert(),
+                item_list['insert'].values()
+            )
+        
+        print '%0.2fs | %0.2fs | %d | %d | %s' % (
+            time.time() - time_start, 
+            time.time() - time_delta, 
+            len(item_list['insert']),
+            len(item_list['update']), 
+            region.name
+        )
 
-        greenlet_pool.join()
-        for greenlet in greenlet_list:
-            if greenlet.value:
-                db.engine.execute(
-                    raw_sql_query % (
-                        ItemPrice.__tablename__,
-                        greenlet.value['item_id'],
-                        greenlet.value['region_id'],
-                        greenlet.value['sell_price'],
-                        greenlet.value['buy_price'],
-                        greenlet.value['sell_price'],
-                        greenlet.value['buy_price'],
-                    )
-                )
-
-        greenlet_list = []
