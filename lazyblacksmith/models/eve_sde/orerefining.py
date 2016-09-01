5bd868984aa474f1eb51d441fa6c73c300db626d
Kyria <anakhon@gmail.com>
2016-09-01 17:59:40 +0200

    Add ice compress and ice flag, and batchsize in ore refining

--- lazyblacksmith/models/eve_sde/orerefining.py
+++ lazyblacksmith/models/eve_sde/orerefining.py
@@ -5,4 +5,7 @@ from . import db
 class OreRefining(db.Model):
     ore_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)   
     material_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
-    quantity = db.Column(db.Integer, nullable=True)
+    quantity = db.Column(db.Integer)
+    batch = db.Column(db.Integer)
+    is_compressed = db.Column(db.Boolean, default=False)
+    is_ice = db.Column(db.Boolean, default=False)
