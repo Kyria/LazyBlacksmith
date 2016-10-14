82fba4a9e003300e7456da90a6b8601823764476
Kyria <anakhon@gmail.com>
2016-10-14 09:40:59 +0200

    add optionnal url argument for the sqlite sde conversion

--- update_sde.sh
+++ update_sde.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ $# -gt 0 ]]
+then
+    url=$1
+    filename=${url##*/}
+    filename=${filename%%.bz2}
+else
+    url=https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2
+    filename=sqlite-latest.sqlite
+fi
+
 command -v bunzip2 >/dev/null 2>&1
 if [[ $? -ne 0 ]]
 then
@@ -8,22 +18,22 @@ then
 fi 
 
 echo -en "Downloading latest sqlite SDE from fuzzwork.co.uk ... "
-wget https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2 >/dev/null 2>&1
+wget $url >/dev/null 2>&1
 if [[ $? -ne 0 ]]
 then
     echo "[FAILED]"
     echo "An error occured, please download manually latest sqlite SDE here https://www.fuzzwork.co.uk/dump"
-    echo "Then run 'python manage.py sde_import -d sqlite-latest.sqlite' after unzipping it"
+    echo "Then run 'python manage.py sde_import -d $filename' after unzipping it"
     exit 2
 else
     echo "[SUCCESS]"
     echo -ne "Unzipping file..."
-    bunzip2 -f sqlite-latest.sqlite.bz2 >/dev/null 2>&1
+    bunzip2 -f ${filename}.bz2 >/dev/null 2>&1
     if [[ $? -ne 0 ]]
     then
         echo "[FAILED]"
         echo "Something wrong happend, please unzip manually the file"
-        echo "Then run 'python manage.py sde_import -d sqlite-latest.sqlite'"
+        echo "Then run 'python manage.py sde_import -d $filename'"
     else
 	echo "[SUCCESS]"
     fi
@@ -34,8 +44,8 @@ python -c 'import lazyblacksmith.app' >/dev/null 2>&1
 if [[ $? -eq 0 ]]
 then
     echo "Importing SDE"
-    python manage.py sde_import -d sqlite-latest.sqlite
+    python manage.py sde_import -d $filename
 else
     echo "You are not in the virtualenv for lazyblacksmith"
-    echo "Please activate your virtualenv and run 'python manage.py sde_import -d sqlite-latest.sqlite'"
+    echo "Please activate your virtualenv and run 'python manage.py sde_import -d $filename'"
 fi
