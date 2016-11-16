792857c52373b05a44986a426f7ad4666df1aac9
Kyria <anakhon@gmail.com>
2016-11-16 08:59:02 +0100

    fix sso callback

--- lazyblacksmith/views/sso.py
+++ lazyblacksmith/views/sso.py
@@ -53,7 +53,7 @@ def crest_callback():
     session['refresh_token'] = auth_response['refresh_token']
 
     crest_auth = get_crest()
-    crest_auth.temptoken_authorize(
+    crest_auth = crest_auth.temptoken_authorize(
         auth_response['access_token'],
         auth_response['expires_in'],
         auth_response['refresh_token']
