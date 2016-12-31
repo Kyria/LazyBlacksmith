40de3fa8afcfcfc0621f26ec3d41cabb1539a0d8
Guillaume <anakhon@gmail.com>
2016-12-31 16:58:36 +0100

    Change some celery related stuff

--- celery_app.py
+++ celery_app.py
@@ -10,24 +10,24 @@ app.app_context().push()
 
 celery_app.init_app(app)
 
-celery_app.conf.update({
-    'CELERYBEAT_SCHEDULE': {
-        'adjusted-price': {
-            'task': 'schedule.update_adjusted_price',
-            'schedule': crontab(hour=1, minute=30),
-        },
-        'item-market-price': {
-            'task': 'schedule.update_market_price',
-            'schedule': crontab(hour='*/2', minute=00),
-        },
-        'industry-index': {
-            'task': 'schedule.update_industry_indexes',
-            'schedule': crontab(hour='*', minute=00),
-        },
+celery_app.conf.broker_url = config.broker_url
+celery_app.conf.beat_schedule.update({
+    'adjusted-price': {
+        'task': 'schedule.update_adjusted_price',
+        'schedule': crontab(hour=1, minute=30),
     },
-    'CELERY_IMPORTS': [
-        'lazyblacksmith.tasks.adjusted_price',
-        'lazyblacksmith.tasks.market_order',
-        'lazyblacksmith.tasks.industry_index',
-    ]
+    'item-market-price': {
+        'task': 'schedule.update_market_price',
+        'schedule': crontab(hour='*/2', minute=00),
+    },
+    'industry-index': {
+        'task': 'schedule.update_industry_indexes',
+        'schedule': crontab(hour='*', minute=00),
+    },
 })
+
+celery_app.conf.imports = [
+    'lazyblacksmith.tasks.adjusted_price',
+    'lazyblacksmith.tasks.market_order',
+    'lazyblacksmith.tasks.industry_index',
+]
