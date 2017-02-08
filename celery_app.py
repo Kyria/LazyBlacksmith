d7886396d96f3705b6ef6268b430ae200624a63a
Kyria <anakhon@gmail.com>
2017-02-08 16:18:26 +0100

    change celery_app to have the new tasks and cron

--- celery_app.py
+++ celery_app.py
@@ -18,23 +18,20 @@ celery_app.init_app(app)
 
 #celery_app.conf.broker_url = config.broker_url
 celery_app.conf.beat_schedule.update({
-    'adjusted-price': {
-        'task': 'schedule.update_adjusted_price',
-        'schedule': crontab(hour=1, minute=30),
+    'character-task-spawner': {
+        'task': 'schedule.character_task_spawner',
+        'schedule': crontab(minute='*'),
     },
-    'item-market-price': {
-        'task': 'schedule.update_market_price',
-        'schedule': crontab(hour='*/2', minute=00),
-    },
-    'industry-index': {
-        'task': 'schedule.update_industry_indexes',
-        'schedule': crontab(hour='*', minute=00),
+    'universe-task-spawner': {
+        'task': 'schedule.universe_task_spawner',
+        'schedule': crontab(minute='*/30'),
     },
 })
 
 celery_app.conf.imports = [
-    #'lazyblacksmith.tasks.market.adjusted_price',
-    #'lazyblacksmith.tasks.market.market_order',
-    #'lazyblacksmith.tasks.industry.indexes',
+    'lazyblacksmith.tasks.task_spawner',
+    'lazyblacksmith.tasks.market.adjusted_price',
+    'lazyblacksmith.tasks.market.market_order',
+    'lazyblacksmith.tasks.industry.indexes',
     'lazyblacksmith.tasks.character.character_skills'
 ]
