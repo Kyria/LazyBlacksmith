649c9c1cb879c0b60f5a39a5dacbcf7e71946d27
Kyria <anakhon@gmail.com>
2018-11-30 10:59:31 +0000

    remove unused methods

--- lazyblacksmith/models/sde/activity.py
+++ lazyblacksmith/models/sde/activity.py
@@ -7,11 +7,3 @@ class Activity(db.Model):
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
     time = db.Column(db.Integer, nullable=True)
     activity = db.Column(db.Integer, primary_key=True, autoincrement=False)
-
-    @classmethod
-    def get_activity_name(cls, activity):
-        return cls.ACTIVITIES[activity]
-
-    @classmethod
-    def check_activity_existence(cls, activity):
-        return activity in cls.ACTIVITIES
