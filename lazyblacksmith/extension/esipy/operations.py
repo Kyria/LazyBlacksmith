5e285b3ee2af735ac73135251a280f6293032ab1
Guillaume BISCH <anakhon@gmail.com>
2022-05-19 01:56:37 +0200

    update operation to avoid deprecated

--- lazyblacksmith/extension/esipy/operations.py
+++ lazyblacksmith/extension/esipy/operations.py
@@ -12,8 +12,8 @@ get_markets_region_id_orders = esiapp.get_v1_swagger.op['get_markets_region_id_o
 # character endpoints
 get_characters = esiapp.get_v5_swagger.op['get_characters_character_id']
 get_characters_skills = esiapp.get_v4_swagger.op['get_characters_character_id_skills']
-get_characters_blueprints = esiapp.get_v2_swagger.op['get_characters_character_id_blueprints']
+get_characters_blueprints = esiapp.get_v3_swagger.op['get_characters_character_id_blueprints']
 
 # corporation related endpoints
-get_characters_roles = esiapp.get_v2_swagger.op['get_characters_character_id_roles']
-get_corporations_blueprints = esiapp.get_v2_swagger.op['get_corporations_corporation_id_blueprints']
+get_characters_roles = esiapp.get_v3_swagger.op['get_characters_character_id_roles']
+get_corporations_blueprints = esiapp.get_v3_swagger.op['get_corporations_corporation_id_blueprints']
